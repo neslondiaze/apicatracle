39a43487b3b17557757864c661dede3a01f02714
neslondiaze <neslondiaze@gmail.com>
2024-05-02 00:29:24 -0400

    cambio de nombre user a auth

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import "./database/connectdb.js";
 import express from "express";
 import morgan from "morgan";
 import homeRouter from "./routes/home.route.js";
-import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1", homeRouter);
-app.use("/api/v1", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🔥🔥🔥 http://localhost:${PORT}`));
